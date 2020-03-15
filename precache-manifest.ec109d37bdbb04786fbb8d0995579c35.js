51a19ac53d868d1a50ec6a42d21ec618d90122b2
Steven Sim <stevensim226@gmail.com>
2020-03-16 00:27:19 +0700

    Updates

---
+++ precache-manifest.ec109d37bdbb04786fbb8d0995579c35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afd3ebde42200f401f00f9e95aec50a2",
+    "url": "/compfest-entry/index.html"
+  },
+  {
+    "revision": "ccdd5fa6808e13649665",
+    "url": "/compfest-entry/static/css/main.e7282bb8.chunk.css"
+  },
+  {
+    "revision": "a6d8df909cb52d2d95ff",
+    "url": "/compfest-entry/static/js/2.c655fb2b.chunk.js"
+  },
+  {
+    "revision": "5d02016f6a9a3fbb9bc638d50f0a8b90",
+    "url": "/compfest-entry/static/js/2.c655fb2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccdd5fa6808e13649665",
+    "url": "/compfest-entry/static/js/main.fe1fd8aa.chunk.js"
+  },
+  {
+    "revision": "381e5cae3b50457093e5",
+    "url": "/compfest-entry/static/js/runtime-main.b7c0dd96.js"
+  }
+]);
